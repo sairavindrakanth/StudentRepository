3488fd5857fb628329675d1b8f40e3ae8f1d5bc7
sai <sairavindrakanth@gmail.com>
2020-04-25 17:14:02 +0530

    added age of the student by TL

--- StudentInfo/src/com/students/Student1.java
+++ StudentInfo/src/com/students/Student1.java
@@ -10,5 +10,6 @@ public class Student1 {
 		System.out.println("interested course is : Java");
 		System.out.println("Student Address is : Banglore");
 		System.out.println("perminent address : Nandyal");
+		System.out.println("student age is : 24");
 	}
 }
