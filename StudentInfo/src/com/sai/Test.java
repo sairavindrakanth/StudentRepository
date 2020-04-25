0d368429974631578b929dcae8a383369b8257c9
sai <sairavindrakanth@gmail.com>
2020-04-25 16:52:42 +0530

    first import of the project by TL

---
+++ StudentInfo/src/com/sai/Test.java
@@ -0,0 +1,12 @@
+package com.sai;
+
+import com.students.Student1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Student1 s1 = new Student1();
+		s1.getStudent();
+	}
+
+}
---
+++ StudentInfo/src/com/students/Student1.java
@@ -0,0 +1,8 @@
+package com.students;
+
+public class Student1 {
+	public void getStudent()
+	{
+		System.out.println("new java student is : SAI");
+	}
+}
