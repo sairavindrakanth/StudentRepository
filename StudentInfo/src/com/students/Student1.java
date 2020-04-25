3bef6f2ab62e1027c321fa0f9c117e524912795f
lakshmikaanth <shivakanth.java94@gmail.com>
2020-04-25 17:16:20 +0530

    added yeas of experience by Devoloper

--- StudentInfo/src/com/students/Student1.java
+++ StudentInfo/src/com/students/Student1.java
@@ -11,5 +11,6 @@ public class Student1 {
 		System.out.println("Student Address is : Banglore");
 		System.out.println("perminent address : Nandyal");
 		System.out.println("student age is : 24");
+		System.out.println("student working exeperience : 2 years");
 	}
 }
